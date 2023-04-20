d5124645dcbaf61c452cf305acc89814cb5b83b9
Felipe Silva <felipemosilvaa@gmail.com>
2023-04-20 00:38:01 -0300

    fix removing token utils

--- src/utils/token.ts
+++
@@ -1,5 +0,0 @@
-export const getToken = () => localStorage.getItem('@token')
-
-export const setToken = (token: string) => localStorage.setItem('@token', token)
-
-export const clearToken = () => localStorage.removeItem('@token')
